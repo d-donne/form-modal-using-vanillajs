4930082c99c6858bc1281c0112f959f2ac267d87
@d-donne <doublyd@gmail.com>
2023-09-08 23:32:03 +0000

    added javascript and animation

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let openBtn = document.getElementById('openBtn');
-let modal = document.getElementById('modal');
-let closeModal = document.getElementById('close_modal');
+let openBtn = document.getElementById("openBtn");
+let modalContainer = document.querySelector(".modal-container");
+let modal = document.getElementById("modal");
+let closeModal = document.getElementById("close_modal");
 
-openBtn.addEventListener('click', function(){
-    modal.style.display = 'flex';
-    openBtn.style.display = 'none';
-})
+openBtn.addEventListener("click", function () {
+  modal.style.display = "flex";
+  openBtn.style.display = "none";
+});
 
-closeModal.addEventListener('click', function(){
-    modal.style.display = 'none';
+closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+
+  openBtn.style.display = "block";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === document.querySelector("body")) {
+    modal.style.display = "none";
     openBtn.style.display = "block";
+  }
 });
